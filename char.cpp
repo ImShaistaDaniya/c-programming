57e62b8d628e1fe941a23c29fbcc507139a300cd
Shaista_Daniya <85442187+ImShaistaDaniya@users.noreply.github.com>
2022-01-15 21:37:43 +0530

    Add files via upload

---
+++ char.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  char ch;
+  cout<<"enter any alphabet"<<endl;
+  cin>>ch;
+  if(ch=='s'||ch=='S')
+  cout<<"SHAISTA DANIYA"<<endl;
+  else if(ch=='h'||ch=='S')
+  cout<<"SHAISTA DANIYA"<<endl;
+  else if(ch=='a'||ch=='A')
+  cout<<"SHAISTA DANIYA"<<endl;
+  else if(ch=='i'||ch=='I')
+  cout<<"SHAISTA DANIYA"<<endl;
+  else if(ch=='t'||ch=='T')
+  cout<<"SHAISTA DANIYA"<<endl;
+  else if(ch=='d'||ch=='D')
+  cout<<"SHAISTA DANIYA"<<endl;
+  else if(ch=='n'||ch=='N')
+  cout<<"SHAISTA DANIYA"<<endl;
+  else if(ch=='y'||ch=='Y')
+  cout<<"SHAISTA DANIYA"<<endl;
+ else
+ cout<<"you have entered something wrong"<<endl;
+
+
+  
+}
