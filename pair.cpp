a1d06dd70e11fa7996f64dcffbf2d311defd2e2d
Shaista_Daniya <85442187+ImShaistaDaniya@users.noreply.github.com>
2022-04-06 13:34:47 +0530

    Add files via upload

---
+++ pair.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,pair;
+    cout<<"enter no.of items";
+    cin>>n;
+     while(n--)
+    {
+        cout<<"pair of";
+        cin>>pair;
+        cout<<pair+1<<endl;
+    }
+   return 0;  
+}
